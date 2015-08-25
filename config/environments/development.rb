70bbb1383de671ff5026c3c5259ab545737029bb
James Becker <becker.james90@gmail.com>
2015-08-25 16:16:25 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+config.web_console.whitelisted_ips = '98.228.240.97'
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
@@ -35,6 +35,10 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+  
+   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(
+      :email                 => "admin@xxxxx.xxx",
+      :password              => "12345678",
+      :password_confirmation => "12345678"
+  )
